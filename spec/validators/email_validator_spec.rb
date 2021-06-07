9c47a93d8c61109c4686dd3ad2c6589e5af226b4
Timofey Shufletyuk <qotofey@gmail.com>
2021-06-07 16:54:35 +0800

    set up puma

--- spec/validators/email_validator_spec.rb
+++ spec/validators/email_validator_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EmailValidator do
-  context 'when set valid name' do
+  context 'when set valid email' do
     it { expect(described_class.valid?('aaa@aaa.aa')).to be_truthy }
   end
 end
