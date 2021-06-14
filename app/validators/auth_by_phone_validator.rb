a88bd76633e0394c9c2588416a6df2cbef09856a
Timofey Shufletyuk <qotofey@gmail.com>
2021-06-14 22:52:24 +0800

    fix auth by phone

--- app/validators/auth_by_phone_validator.rb
+++ app/validators/auth_by_phone_validator.rb
@@ -1,2 +1,7 @@
 class AuthByPhoneValidator
+  attr_accessor :phone, :password
+  include ActiveModel::Model
+
+  validates :phone, phone: true, presence: true
+  validates :password, presence: true
 end
